5805830838a272b5832010b160a6e2bca4541962
Gustave Rousselet <gustave.rousselet@icloud.com>
2021-08-06 19:04:11 +0200

    2 is a prime number

--- src/main/scala/com/dixa/primes/Primes.scala
+++ src/main/scala/com/dixa/primes/Primes.scala
@@ -11,6 +11,6 @@ object Primes {
   def efficientPrimesUpTo(n: Int): LazyList[Int] = {
     val odds = LazyList.from(3, 2).takeWhile(_ <= Math.sqrt(n).toInt)
     val composites = odds.flatMap(i => LazyList.from(i * i, 2 * i).takeWhile(_ <= n))
-    LazyList.from(3, 2).takeWhile(_ <= n).diff(composites)
+    2 #:: LazyList.from(3, 2).takeWhile(_ <= n).diff(composites)
   }
 }
